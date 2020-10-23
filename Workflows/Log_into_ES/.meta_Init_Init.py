6180333c3d3ffb40b86d006dcaeff1d112be7c2b
ubihgu <32392787+ubihgu@users.noreply.github.com>
2020-10-23 12:10:25 +0200

    Log to ES WF

--- Workflows/Log_into_ES/.meta_Init_Init.py
+++ Workflows/Log_into_ES/.meta_Init_Init.py
@@ -3,7 +3,7 @@
     <map>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1603442540070</value>
+            <value>1603445291298</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -11,7 +11,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1603442540062</value>
+            <value>1603445291288</value>
         </entry>
         <entry>
             <key>TAG</key>
