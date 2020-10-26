37f027545f7d52d330ab94e4d393f009e2e49787
ubihgu <32392787+ubihgu@users.noreply.github.com>
2020-10-26 17:02:14 +0100

    remove hgu test

--- Scripts/ubi.sh
+++ Scripts/ubi.sh
@@ -6,8 +6,13 @@ echo "key is $key"
 
 echo "--------------------------------------------------"
 
-curl --insecure -H 'Accept: application/json' -H "Authorization: Bearer $key" -XGET https://127.0.0.1/ubi-api-rest/lookup/devices | python -m json.tool
+#curl --insecure -H 'Accept: application/json' -H "Authorization: Bearer $key" -XGET https://127.0.0.1/ubi-api-rest/lookup/devices | python -m json.tool
 
-curl --insecure -H 'Accept: application/json' -H "Authorization: Bearer $key" -XGET https://127.0.0.1/ubi-api-rest/elastic-search/v1/index-settings | python -m json.tool
+#curl --insecure -H 'Accept: application/json' -H "Authorization: Bearer $key" -XGET https://127.0.0.1/ubi-api-rest/elastic-search/v1/index-settings | python -m json.tool
 
-curl --insecure -H 'Accept: application/json' -H "Authorization: Bearer $key" -XPOST https://127.0.0.1/ubi-api-rest/elastic-search/v1/logs | python -m json.tool
+#curl --insecure -H 'Accept: application/json' -H "Authorization: Bearer $key" -XPOST https://127.0.0.1/ubi-api-rest/elastic-search/v1/logs | python -m json.tool
+
+#curl --insecure -H 'Accept: application/json' -H "Authorization: Bearer $key" -XGET https://127.0.0.1/ubi-api-rest/orchestration/v1/services -d '{"ubiqubeId":"BLRA6"}' | python -m json.tool
+
+curl --insecure -H 'Accept: application/json' -H "Authorization: Bearer $key" -XGET https://127.0.0.1/ubi-api-rest/orchestration/BLRA6/service/instance | python -m json.tool
+
